d7c897c605e19130590093353925519a6d2e7051
Tofunmi Osho <oshotofunmi@gmail.com>
2020-12-27 14:12:25 +0100

    feat: implement login function

--- client/pages/login/index.js
+++ client/pages/login/index.js
@@ -1,14 +1,18 @@
 import { useState } from 'react';
 import classes from './login.module.css';
 import Button from '../../components/UI/Button/Button';
-import Link from 'next/link'
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 const login = () => {
   const [state, setState] = useState({
     email: '',
     password: '',
-    error: {}
+    error: null,
+    loading: false
   })
+
+  const router = useRouter();
 
   const inputChangeHandler = (event) => {
     setState({
@@ -18,14 +22,50 @@ const login = () => {
   }
 
 
-  const loginHandler = (event) => {
+  const loginHandler = async (event) => {
     event.preventDefault();
-    console.log('Attemting login');
+    setState({
+      ...state,
+      loading: true
+    })
+    const details = {
+      email: state.email,
+      password: state.password
+    }
+
+    try {
+      // Attempt log in
+      const res = await fetch('http://localhost:5000/api/user/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(details)
+      })
+
+      // Check if response is ok and throw error if not
+      if(!res.ok) {
+        const error = await res.clone().json();
+        throw error;
+      }
+
+      const data = await res.json();
+      console.log(data);
+      localStorage.setItem('auth-token', data);
+      setState({ ...state, loading: false, error: null });
+      router.push('/shop');
+    } catch(err) {
+      console.log(err);
+      setState({ ...state, loading: false, error: err.error });
+    }
   }
 
   return (
     <div className={classes.login}>
       <h2 className={classes.header}>Login</h2>
+      {
+        state.error && (<p className={classes.error}>{state.error}</p>)
+      }
       <form onSubmit={loginHandler} className={classes.loginForm}>
         <div className={classes.formGroup}>
           <label>Email</label>
@@ -33,6 +73,7 @@ const login = () => {
             type="email" 
             name="email"
             value={state.email}
+            required
             onChange={inputChangeHandler}
           />
         </div>
@@ -42,13 +83,17 @@ const login = () => {
             type="password" 
             name="password"
             value={state.password}
+            required
             onChange={inputChangeHandler}
           />
         </div>
-        <Button type="submit" btnClassName={classes.submitBtn}>Log In</Button>
-        <p className={classes.signup}>Don't have an account yet? <Link href="/signup"><a>Sign up</a></Link></p>
+        <Button 
+          type="submit" 
+          btnClassName={classes.submitBtn} 
+          disabled={state.loading ? true : false}
+        >Log In</Button>
       </form>
-      
+      <p className={classes.signup}>Don't have an account yet? <Link href="/signup"><a>Sign up</a></Link></p>
     </div>
   )
 }
