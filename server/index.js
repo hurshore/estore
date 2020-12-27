02be7c8f13b75630296c357bb897b9148fa9a7a6
Tofunmi Osho <oshotofunmi@gmail.com>
2020-12-27 12:57:50 +0100

    feat: enable cors on the server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const cors = require('cors');
 const authRoutes = require('./routes/authRoutes');
 const productRoutes = require('./routes/productRoutes');
 const cartRoutes = require('./routes/cartRoutes');
@@ -16,6 +17,7 @@ mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology
 });
 
 app.use(express.json());
+app.use(cors());
 
 // Route middleware
 app.use('/api/user', authRoutes);
