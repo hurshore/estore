06102217faa41110182a2c6824b3482f649887aa
Tofunmi Osho <oshotofunmi@gmail.com>
2021-01-02 22:48:23 +0100

    chore: remove 'upload product' endpoint

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { uploadProduct, getProducts, getProduct } = require('../controllers/productController');
 
-router.post('/upload', uploadProduct);
+// router.post('/upload', uploadProduct);
 router.get('/', getProducts);
 router.get('/:productId', getProduct);
 
