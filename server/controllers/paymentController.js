342fef34c1e06eefeff9fb1bdf4a9d7d39ade522
Tofunmi Osho <oshotofunmi@gmail.com>
2021-01-04 23:57:01 +0100

    bug: fix undefined payment intent amount

--- server/controllers/paymentController.js
+++ server/controllers/paymentController.js
@@ -1,22 +1,22 @@
 const stripe = require("stripe")(process.env.STRIPE_SK_TEST);
 const Cart = require('../models/Cart');
 
-const calculateOrderAmount = async () => {
+const calculateOrderAmount = async (uid) => {
   // Find the user's cart
-  const cart = await Cart.findOne({ user: req.user._id });
+  const cart = await Cart.findOne({ user: uid });
   if(!cart) return res.status(400).json({ error: 'Your cart is empty' });
   
   return cart.total;
 };
 
 const createPaymentIntent = async (req, res) => {
-  const { items } = req.body;
   // Create a PaymentIntent with the order amount and currency
+  const amount = await calculateOrderAmount(req.user._id);
   const paymentIntent = await stripe.paymentIntents.create({
-    amount: calculateOrderAmount(items),
+    amount,
     currency: "usd"
   });
-  res.send({
+  res.json({
     clientSecret: paymentIntent.client_secret
   });
 }
--- server/routes/paymentRoutes.js
+++ server/routes/paymentRoutes.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const { createPaymentIntent } = require('../controllers/paymentController');
+const authMiddleware = require('../middleware/auth');
 
-router.post("/create-payment-intent", createPaymentIntent);
+router.get("/create-payment-intent", authMiddleware, createPaymentIntent);
 
 module.exports = router;
