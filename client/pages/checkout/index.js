7a094899fc84465799166b71905cb0abfe88f94b
Tofunmi Osho <oshotofunmi@gmail.com>
2021-01-08 22:31:45 +0100

    feat: make checkout page a private route

--- client/pages/checkout/index.js
+++ client/pages/checkout/index.js
@@ -1,7 +1,7 @@
-import React from "react";
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from "@stripe/react-stripe-js";
 import CheckoutForm from "../../components/Checkout/CheckoutForm";
+import withAuth from '../../hoc/withAuth';
 const promise = loadStripe("pk_test_51I5YXiJgUlScySzFTK5HgWxnx8tq6ldtkL8bfXo6BjdFnOCP0LHo3IB7z6okBRJS7kccsDV80oLoR9AnDNTEGt3v00ItR1OdyC");
 
 const checkout = () => {
@@ -14,4 +14,6 @@ const checkout = () => {
   );
 }
 
-export default checkout;
+// export default checkout;
+
+export default withAuth(checkout);
