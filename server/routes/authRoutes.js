448d5c5efa4f8dde4628ad33b5afa06fd2eab7f2
Tofunmi Osho <oshotofunmi@gmail.com>
2021-01-16 16:29:08 +0100

    chore: changing folder structure

--- server/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { signup, login } = require('../controllers/authController');
-
-router.post('/signup', signup);
-router.post('/login', login);
-
-module.exports = router;
--- server/routes/paymentRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { createPaymentIntent } = require('../controllers/paymentController');
-const authMiddleware = require('../middleware/auth');
-
-router.get("/create-payment-intent", authMiddleware, createPaymentIntent);
-
-module.exports = router;
